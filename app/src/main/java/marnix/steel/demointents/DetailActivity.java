1fd370e609e38aa3ba5df2195ece7da3724b7923
marnixsteel <marnix.steel@icloud.com>
2018-02-15 14:21:52 +0100

    completed detail page from name spinner

--- app/src/main/java/marnix/steel/demointents/DetailActivity.java
+++ app/src/main/java/marnix/steel/demointents/DetailActivity.java
@@ -2,12 +2,19 @@ package marnix.steel.demointents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private TextView tvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        tvName = findViewById(R.id.tv_detailsName);
+        String passedName = getIntent().getStringExtra("name");
+        tvName.setText(passedName);
     }
 }
