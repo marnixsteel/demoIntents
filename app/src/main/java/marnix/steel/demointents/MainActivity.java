7cc4fea18d9362c72f85882f1faa44c83f140fdb
marnixsteel <marnix.steel@icloud.com>
2018-02-15 11:29:19 +0100

    btn.setonclicklistener

--- app/src/main/java/marnix/steel/demointents/MainActivity.java
+++ app/src/main/java/marnix/steel/demointents/MainActivity.java
@@ -2,12 +2,50 @@ package marnix.steel.demointents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCall, btnHomepage, btnMap, btnDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCall = findViewById(R.id.btn_call);
+        btnHomepage = findViewById(R.id.btn_homepage);
+        btnMap = findViewById(R.id.btn_map);
+        btnDetails = findViewById(R.id.btn_details);
+
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnHomepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
